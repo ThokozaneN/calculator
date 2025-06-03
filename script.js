e5b5d9a171f42d99b0ee945a5f3690d1b32dab5a
Thokozane Nxumalo <nxuthokozane@gmail.com>
2025-06-03 14:59:03 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+var newLine = true; //Boolean variable. Determines if the next thing the user types should be on a new line
+
+//Event Handler For When Any Button or Digit Is Pressed
 function digitBtnPressed(button) {
-    var currentValue = document.getElementById("inputBox").value;
-    document.getElementById("inputBox").value = currentValue + button;
+    if(newLine){
+        document.getElementById("inputBox").value = button;
+        newLine = false;
+    } else {
+        var currentValue = document.getElementById("inputBox").value;
+        document.getElementById("inputBox").value = currentValue + button;
+    }
+}
+
+//AC Button Event Handler
+function btnACPressed() {
+    document.getElementById("inputBox").value = 0;
+    newLine = true;
 }
