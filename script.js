4af6a67c4ab3cd87744ee137adc690b959e0c092
Thokozane Nxumalo <nxuthokozane@gmail.com>
2025-06-03 16:23:51 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var newLine = true; //Boolean variable. Determines if the next thing the user types should be on a new line
+var value1;
+var currentOperator;
 
 //Event Handler For When Any Button or Digit Is Pressed
 function digitBtnPressed(button) {
@@ -15,4 +17,35 @@ function digitBtnPressed(button) {
 function btnACPressed() {
     document.getElementById("inputBox").value = 0;
     newLine = true;
+}
+
+//Event Handler for thee operators (+, -, /)
+function operatorBtnPressed(operator) {
+    currentOperator = operator;
+    value1 = parseInt(document.getElementById("inputBox").value);
+    newLine = true;
+}
+
+//Event handler for Equals 
+function equalsBtnPressed() {
+    var value2 = parseInt(document.getElementById("inputBox").value);
+    var finalTotal;
+
+    switch(currentOperator) {
+        case "+":
+            finalTotal = value1 + value2
+            break;
+        case "-":
+            finalTotal = value1 - value2
+            break;
+        case "/":
+            finalTotal = value1 / value2
+            break;
+        case "*":
+            finalTotal = value1 * value2
+            break;
+    }
+    document.getElementById("inputBox").value = finalTotal;
+    value1 = 0;
+    newLine =true;
 }
